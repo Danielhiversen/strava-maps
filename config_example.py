09ee45fcebb62584c885f652a3d8b0edb5dc5e88
Daniel Iversen <Daniel.Iversen@tibber.com>
2018-06-23 13:00:49 +0200

    activity map, and altitude plot

---
+++ config_example.py
@@ -0,0 +1,7 @@
+#
+# "Client-ID" and "Client-Secret" from https://www.strava.com/settings/api
+#
+CLIENT_ID     = '19661'
+CLIENT_SECRET = '673409cdf6d02b8bc47b0e88cd03015283dddba2'
+
+AUTH_URL      = 'http://127.0.0.1:7123/auth'
---
+++ run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+from src import app
+app.run(port=7123, debug=True)
